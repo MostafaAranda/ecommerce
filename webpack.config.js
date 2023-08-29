bb104305c2d6e0cbec33f8145926227b25425f3d
MostafaArandas <mostafaaarandas@gmail.com>
2023-08-29 13:03:19 +0300

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,91 @@
+var path = require('path');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+var MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
+
+module.exports = {
+  entry: {
+    app: './src/index.js',
+  },
+
+  output: {
+    path: path.join(__dirname, '/dist'),
+    publicPath: '',
+    filename: 'main.js',
+    clean: true,
+  },
+
+  mode: 'development',
+
+  devServer: {
+    static: {
+      directory: path.join(__dirname, 'public'),
+    },
+    hot: false,
+    port: 9000,
+    open: true,
+    devMiddleware: {
+      writeToDisk: true,
+    },
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.html$/i,
+        loader: 'html-loader',
+        options: {
+          minimize: true,
+        },
+      },
+      {
+        test: /\.css$/i,
+        exclude: /bootstrap\.min\.css$/i,
+        use: [MiniCssExtractPlugin.loader, 'css-loader'],
+      },
+
+      {
+        test: /bootstrap\.min\.css$/i,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+            options: {
+              esModule: false,
+            },
+          },
+          'rtlcss-loader',
+        ],
+      },
+
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+        generator: {
+          filename: './images/[name][ext]',
+        },
+      },
+      {
+        test: /\.(svg|eot|woff|woff2|ttf)$/i,
+        type: 'asset/resource',
+        generator: {
+          filename: './fonts/[name][ext]',
+        },
+      },
+    ],
+  },
+
+  plugins: [
+    new HtmlWebpackPlugin({
+      filename: 'index.html',
+      template: './src/index.html',
+    }),
+    new MiniCssExtractPlugin({
+      filename: 'css/style.css',
+    }),
+    // new CssMinimizerPlugin(),
+  ],
+  optimization: {
+    minimizer: [new CssMinimizerPlugin()],
+    minimize: true,
+  },
+};
