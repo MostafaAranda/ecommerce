da6ba2b27c05a08f8efee241febf67aa46796890
MostafaArandas <mostafaaarandas@gmail.com>
2023-09-06 19:59:17 +0300

    brand products done

--- src/index.js
+++ src/index.js
@@ -9,3 +9,8 @@ import '@fortawesome/fontawesome-free/js/all.min';
 
 document.querySelectorAll('[data-bs-toggle="tooltip"]').forEach(item => new bootstrap.Tooltip(item))
 
+document.querySelectorAll('.add-to-cart-btn').forEach(item => {
+    item.addEventListener("click" , () =>{
+        alert("أضيف المنتج إلى عربة الشراء")
+    })
+})
